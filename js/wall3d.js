a28bb53cbf3a0e03040cd810dfb6523c513329ce
sseungjae <61439454+sseungjae@users.noreply.github.com>
2021-01-09 16:03:56 +0900

    wall move
    translateZ
    move wall function

--- js/wall3d.js
+++ js/wall3d.js
@@ -4,7 +4,9 @@
     let maxScrollValue = document.body.offsetHeight - window.innerHeight;
  
     window.addEventListener('scroll', function(){
-        console.log(pageYOffset / maxScrollValue);
+        const zMove = pageYOffset / maxScrollValue * 980 - 490;
+        houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+        console.log(zMove)
     });
-
+ 
 })();
