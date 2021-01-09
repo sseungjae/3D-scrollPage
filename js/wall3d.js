cdc145838aadaf07afb2f78cc1912285a8359b3f
sseungjae <61439454+sseungjae@users.noreply.github.com>
2021-01-09 16:48:58 +0900

    resize function
    responsive window size

--- js/wall3d.js
+++ js/wall3d.js
@@ -3,10 +3,17 @@
     const houseElem = document.querySelector('.house');
     let maxScrollValue = document.body.offsetHeight - window.innerHeight;
  
+    function resizeHandler(){
+        maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    }
+
     window.addEventListener('scroll', function(){
         const zMove = pageYOffset / maxScrollValue * 980 - 490;
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
         console.log(zMove)
     });
  
+    window.addEventListener('resize', resizeHandler);
+
+
 })();
