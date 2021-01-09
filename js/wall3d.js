d963ef1f7895af58f37ea1135050e8c975912de9
sseungjae <61439454+sseungjae@users.noreply.github.com>
2021-01-09 16:58:13 +0900

    add scroll bar
    progressBarControll
    add scrollPersent function

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,19 +1,23 @@
 (function(){
      
     const houseElem = document.querySelector('.house');
-    let maxScrollValue = document.body.offsetHeight - window.innerHeight;
+    const barElem = document.querySelector('.progress-bar');
+    let maxScrollValue;
  
     function resizeHandler(){
         maxScrollValue = document.body.offsetHeight - window.innerHeight;
     }
 
     window.addEventListener('scroll', function(){
-        const zMove = pageYOffset / maxScrollValue * 980 - 490;
+        const scrollPer = pageYOffset / maxScrollValue 
+        const zMove = scrollPer * 980 - 490;
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
-        console.log(zMove)
+        
+        //Progress bar
+        barElem.style.width = scrollPer * 100 + '%';
     });
  
     window.addEventListener('resize', resizeHandler);
-
+    resizeHandler();
 
 })();
