e101ec22ec8b83e031a1bd4322880c3e48abc18a
sseungjae <61439454+sseungjae@users.noreply.github.com>
2021-01-11 21:35:12 +0900

    mouse move with wall
    mouse moving wall moving
    function

--- js/wall3d.js
+++ js/wall3d.js
@@ -1,5 +1,5 @@
 (function(){
-     
+    const stageElem = document.querySelector('.stage'); 
     const houseElem = document.querySelector('.house');
     const barElem = document.querySelector('.progress-bar');
     const mousePos = {x: 0, y: 0};
@@ -10,7 +10,7 @@
     }
 
     window.addEventListener('scroll', function(){
-        const scrollPer = pageYOffset / maxScrollValue 
+        const scrollPer = pageYOffset / maxScrollValue; 
         const zMove = scrollPer * 980 - 490;
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
         
@@ -19,9 +19,9 @@
     });
  
     window.addEventListener('mousemove', function(e) {
-        mousePos.x = -1 +(e.clientX / window.innerWidth) * 2;
-        mousePos.y = 1 +(e.clientY / window.innerHeight) * 2;
-        console.log(mousePos);
+        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+        stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + '0deg)';
     })
 
     window.addEventListener('resize', resizeHandler);
